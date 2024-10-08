c1e0c2d457aade959579752c4e63f4ba9d13b416
afnfyz <dev.afoyez@gmail.com>
2024-10-08 12:42:29 -0400

    initial upload

---
+++ Embedding/app.js
@@ -0,0 +1,16 @@
+// We are creating constants that we will use later on
+const viz = document.getElementById("tableauViz");
+let workbook;
+let vizActiveSheet;
+let dashboard;
+let listSheets;
+
+// Logging information about our workbook
+
+function logWorkbookInformation() {
+    workbook = viz.workbook;
+    console.log(`The workbook name is ${workbook.name}`);
+}
+
+// Run out function, whn the workbook becomes interactive
+viz.addEventListener("firstinteractive",logWorkbookInformation);
